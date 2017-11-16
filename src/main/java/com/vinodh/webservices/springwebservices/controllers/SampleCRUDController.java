61f6e12eb1ec6fa5647a799786445bb0c8c63e9e
vinodh kumar Thimmisetty <vinodh5052@gmail.com>
2017-11-16 05:23:21 -0600

    Controllers update

--- src/main/java/com/vinodh/webservices/springwebservices/controllers/SampleCRUDController.java
+++ src/main/java/com/vinodh/webservices/springwebservices/controllers/SampleCRUDController.java
@@ -1,13 +1,23 @@
 package com.vinodh.webservices.springwebservices.controllers;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,8 +48,50 @@ public class SampleCRUDController {
 		return ResponseEntity.ok(employeeService.findById(employeeId));
 	}
 
-	@GetMapping(value = "/employeeList")
+	@GetMapping(value = "/getAllEmployees")
+	public ResponseEntity<List<Employee>> findAllEmployees() {
+		return ResponseEntity.ok(employeeService.findAllEmployees());
+	}
+
+	@GetMapping(value = "/userslist")
 	public ResponseEntity<List<Employee>> listAllUsers() {
 		return new ResponseEntity<>(employeeService.findAllEmployees(), HttpStatus.OK);
 	}
+
+	@PutMapping(value = "/updateEmployee/{employeeId}")
+	public ResponseEntity<Integer> updateEmployee(@PathVariable("employeeId") long employeeId,
+			@RequestBody Employee employee) {
+		employee.setEmployeeId(employeeId);
+		return ResponseEntity.ok(employeeService.updateEmployee(employee));
+	}
+
+	@PostMapping(value = "/mergeEmployee/{employeeId}")
+	public ResponseEntity<Integer> mergeEmployee(@PathVariable("employeeId") long employeeId,
+			@RequestBody Employee employee) {
+		employee.setEmployeeId(employeeId);
+		return ResponseEntity.ok(employeeService.mergeEmployee(employee));
+
+	}
+
+	@PostMapping(value = "/saveNewEmployee")
+	public ResponseEntity<Integer> saveEmployee(@RequestBody Employee employee) {
+		return ResponseEntity.ok(employeeService.saveEmployee(employee));
+	}
+
+	@DeleteMapping(value = "/deleteEmployee/{employeeId}")
+	public ResponseEntity<Integer> deleteEmployee(@PathVariable("employeeId") long employeeId) {
+		return ResponseEntity.ok(employeeService.deleteEmployeeById(employeeId));
+	}
+
+	@DeleteMapping(value = "/deleteAllEmployees")
+	public ResponseEntity<Integer> deleteAllEmployee() {
+		return ResponseEntity.ok(employeeService.deleteAllEmployees());
+	}
+
+	@InitBinder
+	public void stringTrimming(WebDataBinder binder) {
+		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(new SimpleDateFormat("MM/DD/YYYY"), true));
+	}
+
 }
